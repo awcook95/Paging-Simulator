1a66af9edcde6e7e1d84ba264ae010a0bd1463b3
Alexander Cook <awcook@mail.usf.edu>
2020-02-21 17:45:05 -0500

    initial commit

---
+++ memsim.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+
+    /*
+    scan memory addresses + read or write
+    unsigned addr; 
+    char rw;
+    fscanf(file,"%x %c",&addr,&rw);
+    */
+
+    /*
+    format for quiet output:
+    total memory frames: 12 
+    events in trace: 1002050 
+    total disk reads: 1751 
+    total disk writes: 932 
+    */
+
+    return 0;
+}
