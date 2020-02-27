3f8f58c1fb8745be0e18799a37809c1334ce421c
Alexander Cook <awcook@mail.usf.edu>
2020-02-27 16:10:05 -0500

    I was using the entire VA vs just the VPN, fixed

--- memsim.c
+++ memsim.c
@@ -8,25 +8,113 @@ memsim <tracefile> <nframes> <rdm|lru|fifo|vms> <debug|quiet>
 #include <string.h>
 
 //helper functions
+int find(unsigned elem, unsigned array[], int size){
+    int i;
+    for(i = 0; i < size; i++){
+            if(array[i] == elem)
+            return i;
+        }
+    return -1;
+}
+
 void printoutput(int nframes, int events, int reads, int writes){
-    printf("total memory frames: %d", nframes);
-    printf("events in trace: %d", events);
-    printf("total disk reads: %d", reads);
-    printf("total disk writes: %d", writes);
+    printf("total memory frames: %d\n", nframes);
+    printf("events in trace: %d\n", events);
+    printf("total disk reads: %d\n", reads);
+    printf("total disk writes: %d\n", writes);
+}
+
+int random(int lower, int upper){
+    srand(1);
+    int num = (rand() % (upper - lower + 1)) + lower;
+    return num;
 }
 
 //replacement algorithms
-void rdm(){
-    
+void rdm(FILE* tracefile, int nframes){
+    unsigned* pageTable;
+    char* dirtyBit;
+    pageTable = (unsigned*)malloc(sizeof(unsigned)*nframes);
+    dirtyBit = (char*)malloc(sizeof(char)*nframes);
+    memset(dirtyBit, 'R',sizeof(char)*nframes);
 
+    int capacity = 0; 
+    unsigned addr;
+    char rw;
+    int events = 0;
+    int diskReads = 0;
+    int diskWrites = 0;
+
+    while(fscanf(tracefile,"%x %c",&addr,&rw) != EOF){
+        addr = addr >> 12; //addr is the entire virtual address, we just need the VPN from it so, first 20 bits as each page is size 2^12
+        int i = find(addr, pageTable, nframes); //get index of the page in the page table if it exists
+        if(i != -1){ //if address is in the page table already
+            if(rw == 'W') 
+                dirtyBit[i] = rw;
+        }
+        else if(capacity < nframes){ //adress isn't in the table, is it full?
+            pageTable[capacity] = addr; //add address to the table
+            diskReads++;
+            capacity++;
+            if(rw == 'W') 
+                dirtyBit[i] = rw;
+        }
+        else{ //table is full, must make room
+            i = random(0, nframes); //randomly select page to remove
+            if(dirtyBit[i] == 'W') //if ejected page was dirty write it to disk
+                diskWrites++;
+            pageTable[i] = addr; //add new address to table
+            dirtyBit[i] = rw;
+            diskReads++;
+        }
+        events++;
+    }
+    printoutput(nframes, events, diskReads, diskWrites);
 }
 
 void lru(){
 
 }
 
-void fifo(){
+void fifo(FILE* tracefile, int nframes){
+    unsigned* pageTable;
+    char* dirtyBit;
+    pageTable = (unsigned*)malloc(sizeof(unsigned)*nframes);
+    dirtyBit = (char*)malloc(sizeof(char)*nframes);
+    memset(dirtyBit, 'R',sizeof(char)*nframes);
 
+    int capacity = 0; 
+    unsigned addr;
+    char rw;
+    int events = 0;
+    int diskReads = 0;
+    int diskWrites = 0;
+
+    while(fscanf(tracefile,"%x %c",&addr,&rw) != EOF){
+        addr = addr >> 12; //addr is the entire virtual address, we just need the VPN from it so, first 20 bits as each page is size 2^12
+        int i = find(addr, pageTable, nframes); //get index of the page in the page table if it exists
+        if(i != -1){ //if address is in the page table already
+            if(rw == 'W') 
+                dirtyBit[i] = rw;
+        }
+        else if(capacity < nframes){ //adress isn't in the table, is it full?
+            pageTable[capacity] = addr; //add address to the table
+            diskReads++;
+            capacity++;
+            if(rw == 'W') 
+                dirtyBit[i] = rw;
+        }
+        else{ //table is full, must make room
+            i = random(0, nframes); //randomly select page to remove
+            if(dirtyBit[i] == 'W') //if ejected page was dirty write it to disk
+                diskWrites++;
+            pageTable[i] = addr; //add new address to table
+            dirtyBit[i] = rw;
+            diskReads++;
+        }
+        events++;
+    }
+    printoutput(nframes, events, diskReads, diskWrites);
 }
 
 void vms(){
@@ -57,20 +145,22 @@ int main(int argc, char** argv){
     if((fptr = fopen(tracefile, "r")) == NULL)
         printf("error opening file");
 
-    /*
-    scan memory addresses + read or write
-    unsigned addr; 
-    char rw;
-    fscanf(file,"%x %c",&addr,&rw);
-    */
-
-    /*
-    format for quiet output:
-    total memory frames: 12 
-    events in trace: 1002050 
-    total disk reads: 1751 
-    total disk writes: 932 
-    */
+    //call test function
+    if(strcmp(testType, "rdm") == 0){
+        rdm(fptr, nframes);
+    }
+    else if(strcmp(testType, "lru") == 0){
+        //lru(fptr, nframes);
+    }
+    else if(strcmp(testType, "fifo") == 0){
+        fifo(fptr, nframes);
+    }
+    else if(strcmp(testType, "vms") == 0){
+        //vms(fptr, nframes);
+    }
+    else{
+        printf("unknown page replacement algorithm");
+    }
 
     return 0;
 }
